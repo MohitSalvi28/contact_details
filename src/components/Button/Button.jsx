367d3194feca32c4e41e8799e842cb6720cca2a9
Mohit <msarts1234@gmail.com>
2024-09-29 17:04:14 +0530

    This is the second project

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import { MdMessage } from "react-icons/md";
+import styles from "./Button.module.css";
+const Button = () => {
+  return (
+    <div>
+      <MdMessage />
+      Button
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+import { Container } from "react-bootstrap";
+import styles from "./Navigation.module.css";
+
+const Navigation = () => {
+  return (
+    <nav className={styles.navigation}>
+      <div className="logo ">
+        <img src="/images/logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>CONTACT</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
